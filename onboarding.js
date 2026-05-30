5d24e07cc6c9381e16d15aa94003e3cef8ea6254
Mel <melissapixell@gmail.com>
2026-05-30 12:42:38 +0300

    add const

--- onboarding.js
+++ onboarding.js
@@ -3,3 +3,5 @@ let birthDate = "";        // будет исчисляться возраст �
 let email = "";            // займет значение of user
 let isActive = false;      // if user оплатит подписку -> станет True
 let inviteCount = 0;       // If user to invite друзей -> счетчик+
+
+const FORM_VERSION = "1.0" // неизменяемое значение улетает на сервер
