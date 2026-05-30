3237ef5236287801c0fa2e0d750a9beef99cf191
Mel <melissapixell@gmail.com>
2026-05-30 11:24:41 +0300

    task 'onboarding'
    It is можно считать будущую форму на сайт
    
    My mistakes:
    - использование const. Данные в форме изменяемые, поэтому const не требуется.
    - написание булевого значения с big буквы.

---
+++ onboarding.js
@@ -0,0 +1,5 @@
+let name = "";             // займет значение of user
+let birthDate = "";        // будет исчисляться возраст от даты рождения
+let email = "";            // займет значение of user
+let isActive = false;      // if user оплатит подписку -> станет True
+let inviteCount = 0;       // If user to invite друзей -> счетчик+
