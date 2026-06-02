e62e87c5516f64468ecf4201185eb45d857e9169
Mel <melissapixell@gmail.com>
2026-06-03 00:00:07 +0300

    task if/else

---
+++ free_shipping.js
@@ -0,0 +1,18 @@
+// case: Ты делаешь фичу для интернет-магазина. Маркетинг сказал: "Если корзина больше 5000 рублей, доставка бесплатная. ИЛИ если у пользователя есть промокод 'GIFT', доставка тоже бесплатная. Иначе доставка стоит 500 рублей".
+
+let cartTotal = 4000;
+let hasPromocode = 1;
+let deliveryCost = 0;
+const shippingCost = 500;
+
+if (cartTotal > 5000) {
+    deliveryCost = 1;
+    console.log("Доставка бесплатна!");
+} else if (hasPromocode == true) {
+    deliveryCost = 1;
+    console.log("Доставка бесплатна!");
+}
+else {
+    deliveryCost = 0;
+    console.log(`Стоимость доставки: ${shippingCost}`);
+}
