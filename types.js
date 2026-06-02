b1b031db0037f88159517a1139e9d7906fa11f28
Mel <melissapixell@gmail.com>
2026-06-02 23:14:32 +0300

    Task преобразование типов

---
+++ types.js
@@ -0,0 +1,8 @@
+console.log(true + 2);      // outpout: 3
+console.log(false + 2);     // outpout: 2
+
+// FIRST. data from Back
+let price = "1500";
+let count = "2";
+let sum = +price * +count;
+console.log(sum);
