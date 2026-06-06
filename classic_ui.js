b18b23798de083efa170fba7cae54d04e606f942
Mel <melissapixell@gmail.com>
2026-06-06 19:24:58 +0300

    add logick on boolean value (PATTERN) - create фильтр -> получить честное значение -> скормить терн оператор

--- classic_ui.js
+++ classic_ui.js
@@ -1,6 +1,9 @@
 // case: Пользователь зашел на сайт. Если он авторизован, кнопка в углу должна говорить "Выйти". Если нет — "Войти". Писать ради этого 5 строк if/else — это моветон.
 
-let isLoggedIn = false;
+let isLoggedIn = "false";
 
-let buttonText = isLoggedIn ? "Log out" : "Log in";
+let isLog = (isLoggedIn === true || isLoggedIn === "true");
+console.log(isLog);
+
+let buttonText = isLog ? "Log out" : "Log in";
 console.log(buttonText);
