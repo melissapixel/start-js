e4c4af6c3d2520a24765fb4fd4c4f6ca17056e74
Mel <melissapixell@gmail.com>
2026-06-06 22:45:06 +0300

    task dark mode - color лучше всегда держать в кавычках, как строки

---
+++ dark_mode.js
@@ -0,0 +1,15 @@
+// case: Пользователь кликнул на тумблер "Ночной режим". Нам нужно мгновенно поменять цвет фона страницы.
+
+let isDarkMode = false;
+
+const darkModeColor = "#333333";
+const lightModeColor = "#fff";
+
+let backgroundColor = isDarkMode ? darkModeColor : lightModeColor;
+
+
+console.log(backgroundColor);
+
+
+// Создай переменную backgroundColor. Если isDarkMode равно true, цвет должен быть "#333333" (темно-серый). Если false — "#ffffff" (белый).
+
