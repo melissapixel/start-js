cca35f3fb4fac7cf3932161f94d4c3d79e90468c
Mel <melissapixell@gmail.com>
2026-06-07 13:54:04 +0300

    Task boolean logick

---
+++ access_control2.js
@@ -0,0 +1,8 @@
+// case: Мы не можем показывать кнопку "Удалить сайт" кому попало. Нужно проверить, что пользователь залогинен, у него есть права админа, и он НЕ заблокирован.
+
+let isLoggedIn = true;        // Пользователь вошел в систему
+let isAdmin = false;          // Является ли он админом
+let isBanned = false;         // Забанен ли он
+
+let canSeeAdminPanel = (isLoggedIn === true && isAdmin === true && !isBanned) ? "Can see admin-panel" : "Cant see admin-panel";
+console.log(canSeeAdminPanel);
