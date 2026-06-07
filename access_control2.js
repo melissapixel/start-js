86d0792ab8ec246cd419b3377244dd6738cb258d
Mel <melissapixell@gmail.com>
2026-06-07 13:57:06 +0300

    feat: boolean for UI-component

--- access_control2.js
+++ access_control2.js
@@ -4,5 +4,5 @@ let isLoggedIn = true;        // Пользователь вошел в сист
 let isAdmin = false;          // Является ли он админом
 let isBanned = false;         // Забанен ли он
 
-let canSeeAdminPanel = (isLoggedIn && isAdmin && !isBanned) ? "Can see admin-panel" : "Cant see admin-panel";
+let canSeeAdminPanel = (isLoggedIn && isAdmin && !isBanned) ? true : false;
 console.log(canSeeAdminPanel);
