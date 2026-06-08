89b941740679aef9b0b7608126e8ff22fc312c06
Mel <melissapixell@gmail.com>
2026-06-08 15:05:29 +0300

    task with true/false - get boolean value (FOR СКРЫТОГО КОНТЕНТА) (it is фича)

---
+++ function/acces.js
@@ -0,0 +1,12 @@
+// case: У нас есть контент "18+". Нам нужно быстро проверить, можно ли показать его пользователю, исходя из его возраста.
+
+function checkAgeToContent(age) {
+    if (age >= 18) {
+        return true;
+    }
+    return false;
+}
+
+if (checkAgeToContent(21)) {
+    console.log("Show content +18");
+}
