6c5474bbb018b088005bb85a6875ce57508f6141
Mel <melissapixell@gmail.com>
2026-06-08 17:02:08 +0300

    check on Num

--- function/acces.js
+++ function/acces.js
@@ -1,12 +1,15 @@
 // case: У нас есть контент "18+". Нам нужно быстро проверить, можно ли показать его пользователю, исходя из его возраста.
 
 function checkAgeToContent(age) {
+    if (typeof(age) !== "number") {
+        return false;
+    }
     if (age >= 18) {
         return true;
     }
     return false;
 }
 
-if (checkAgeToContent(21)) {
+if (checkAgeToContent("21")) {
     console.log("Show content +18");
 }
