a06663880f79aefe42f3e9c9b0f3cecaca9801a0
Mel <melissapixell@gmail.com>
2026-06-15 11:53:00 +0300

    add default value

--- function/to_func.js
+++ function/to_func.js
@@ -1,5 +1,6 @@
 // to стрелочная function
 
-const toPower = (num, power) => num ** power;
+const toPower = (num, power = 2) => num ** power;
 
 console.log(toPower(3, 2));
+console.log(toPower(5));
