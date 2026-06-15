4fc534df73ef4ee5e9238b4c27a20659768caa3b
Mel <melissapixell@gmail.com>
2026-06-15 16:11:33 +0300

    task  patching

--- array/introduction.js
+++ array/introduction.js
@@ -2,3 +2,7 @@ let allowedExtensions = ['jpg', 'png', 'webp'];
 
 console.log(allowedExtensions[1]);  // output 2-element
 console.log(allowedExtensions[5]);  // not exist
+
+let userRoles = ['guest', 'guest', 'moderator', 'admin'];
+userRoles[0] = 'reg_user';          // change element
+console.log(userRoles);
