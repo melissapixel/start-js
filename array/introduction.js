73e0bf8325a7599b3d2eb4ca9c63fc8d56580f70
Mel <melissapixell@gmail.com>
2026-06-15 16:17:50 +0300

    task 'tic tac toe'

--- array/introduction.js
+++ array/introduction.js
@@ -6,3 +6,11 @@ console.log(allowedExtensions[5]);  // not exist
 let userRoles = ['guest', 'guest', 'moderator', 'admin'];
 userRoles[0] = 'reg_user';          // change element
 console.log(userRoles);
+
+let play = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+];
+play[1][1] = 'x';
+console.log(play);
