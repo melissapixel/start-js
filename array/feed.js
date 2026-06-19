51bed7e69f6cab2eee414f28205391085b71854d
Mel <melissapixell@gmail.com>
2026-06-19 11:14:51 +0300

    task arrays

---
+++ array/feed.js
@@ -0,0 +1,29 @@
+// При загрузке страницы ты получил историю сообщений из REST API (historyMessages).
+// Пока пользователь читал, прилетели новые сообщения через WebSocket (wsMessages).
+// Тебе нужно склеить их. НО! История от API пришла в старом порядке, а новые сообщения надо добавить в конец. При этом исходный массив historyMessages трогать нельзя (вдруг он используется в другом компоненте!).
+
+// Пришло с API (от старых к новым)
+const historyMessages = [
+  { id: 1, text: 'Старое сообщение' },
+  { id: 2, text: 'Сообщение вчера' }
+];
+
+// Пришло по WebSocket (свежие)
+const wsMessages = [
+  { id: 3, text: 'Привет!' },
+  { id: 4, text: 'Как дела?' }
+];
+
+function getFeed (arrayApi, arrayWebsocket) {
+    // work with reverse
+    const copyArrayApi = arrayApi.slice();
+    copyArrayApi.reverse();
+
+    // склейка
+    return copyArrayApi.concat(arrayWebsocket);
+}
+
+console.log(getFeed(historyMessages, wsMessages));
+
+// ckeck original
+console.log(historyMessages);
