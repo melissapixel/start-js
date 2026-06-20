ef32e3128eb3a97aff0e65fdef08f98b617c91ea
Mel <melissapixell@gmail.com>
2026-06-20 15:44:51 +0300

    steck simple tasks

---
+++ cycle/for.js
@@ -0,0 +1,14 @@
+// case: create счетчик. от 1 до 10
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// case: create счетчик. от 10 до 1
+for (let i = 10; i >=1; i--) {
+    console.log(i);
+}
+
+// case: Выведи строку "Привет!" в консоль 5 раз.
+for (let i = 0; i < 5; i++) {
+    console.log('Hi!');
+}
